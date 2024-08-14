f3a3e29cbcb172030df432ee72315a7b5d17b9f3
Artem Bryzh <aab@AMBM2.local>
2024-08-14 21:26:12 +0100

    Add histori for admin panel

--- expenses/models.py
+++ expenses/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from simple_history.models import HistoricalRecords
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
@@ -13,6 +14,7 @@ class Expense(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     date = models.DateField(blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    history = HistoricalRecords()
 
     def __str__(self):
         return f"{self.amount} - {self.category.name} ({self.user.username})"
