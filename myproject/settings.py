2dbb0ccdcf3ec82e37d4062c9adf18b55feeeddd
Artem Bryzh <aab@AMBM2.local>
2024-08-14 19:41:01 +0100

    Change settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,10 +11,15 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Correct definition of BASE_DIR
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# STATIC_ROOT should use the correct BASE_DIR
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
